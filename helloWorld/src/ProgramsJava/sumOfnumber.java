1e1a0c0f6b737460cd13054c33ab4c8f4491b67d
RISHI <RISHI@DESKTOP-65G0TML>
2023-02-15 18:00:56 +0530

    sum of number is commited

---
+++ helloWorld/src/ProgramsJava/sumOfnumber.java
@@ -0,0 +1,14 @@
+package ProgramsJava;
+
+public class sumOfnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println(+c);
+	}
+
+}
